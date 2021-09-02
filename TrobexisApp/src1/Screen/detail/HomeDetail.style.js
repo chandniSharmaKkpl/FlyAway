9b485a79ed9f40d680f22fb98312cfb91af32c23
chandni <chandni@2excel.com.au>
2021-09-02 14:53:48 +0530

    login api flow correct

--- TrobexisApp/src1/Screen/detail/HomeDetail.style.js
+++
@@ -1,6 +0,0 @@
-export default styles = {
-  container: {
-    width: '100%',
-    height: '100%',
-  },
-};
--- TrobexisApp/src1/context/AuthContext.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-
-export default React.createContext({
-	user: null,
-	setUserData: (val) => {
-		return val
-	}
-})
